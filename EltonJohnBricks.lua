51c4bb4e43874922cdf54ca05d27835ea432d3d3
JumbleBumble <113152254+JumbleBumble@users.noreply.github.com>
2022-11-04 15:36:08 -0400

    Update EltonJohnBricks.lua

--- EltonJohnBricks.lua
+++ EltonJohnBricks.lua
@@ -6,7 +6,7 @@ local toggle = false
 
 
 function Brickos()
-	
+	task.wait()	
 	game:GetService("ReplicatedStorage").InfinityShared.lib.net.Network:FindFirstChild("CollectableBricks:OnPickup"):FireServer("1")
 
 
