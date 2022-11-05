b5b2f4a9712b752380377becc84afd3ae558e360
JumbleBumble <113152254+JumbleBumble@users.noreply.github.com>
2022-11-05 04:25:20 -0400

    Update Trickshot_SimSexyGUI.lua

--- Trickshot_SimSexyGUI.lua
+++ Trickshot_SimSexyGUI.lua
@@ -12,7 +12,10 @@ local UICorner_5 = Instance.new("UICorner")
 local toggle = false
 local toggle2 = false
 local toggle3 = false
-local t = 10;
+
+
+
+
 
 
 
@@ -47,39 +50,37 @@ TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 TextButton.Position = UDim2.new(-0.00377938151, 0, 0.411347866, 0)
 TextButton.Size = UDim2.new(0, 53, 0, 59)
 TextButton.Font = Enum.Font.Arcade
-TextButton.Text = "Inf Cash"
+TextButton.Text = "Get Cash"
 TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
 TextButton.TextScaled = true
 TextButton.TextSize = 14.000
 TextButton.TextWrapped = true
 TextButton.MouseButton1Down:connect(function()
 	toggle = not toggle
+	if toggle == true then
+		TextButton.BackgroundColor3 = Color3.fromRGB(0, 255, 0)
+	end
 	if toggle == false then
 		TextButton.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
 	end
 	while toggle == true do
-	    task.wait()
-	    local hue = tick() % t / t
-	    local colory = Color3.fromHSV(hue,1,1)
-	    if toggle == false then
-		    TextButton.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
-		else
-    	    TextButton.BackgroundColor3 = colory
+		task.wait()
+		for i,v in pairs(game:GetService("Workspace").Tools:GetChildren()) do
     		local args = {
     			[1] = {
-    				[1] = "4095ec2e-5cdd-4a47-85fd-8fa4139a1866"
+    				[1] = v.Name
     			},
     			[2] = {
     				["touched"] = {
-    					[1] = "4095ec2e-5cdd-4a47-85fd-8fa4139a1866"
+    					[1] = v.Name
     				},
     				["distance"] = math.huge
     			}
     		}
     
     		game:GetService("ReplicatedStorage").TShots.Actions.Default.Remotes.Finish:InvokeServer(unpack(args))
+		end
 	end
-end
 end)
 
 
@@ -100,24 +101,20 @@ TextButton_2.TextSize = 14.000
 TextButton_2.TextWrapped = true
 TextButton_2.MouseButton1Down:connect(function()
 	toggle2 = not toggle2
+	if toggle2 == true then
+		TextButton_2.BackgroundColor3 = Color3.fromRGB(0, 255, 0)
+	end
 	if toggle2 == false then
 		TextButton_2.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
 	end
 	while toggle2 == true do
 		task.wait()
-		local huep = tick() % t / t
-	    local coloru = Color3.fromHSV(huep,1,1)
-	    if toggle2 == false then
-		    TextButton_2.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
-		else
-    	    TextButton_2.BackgroundColor3 = coloru
-    		local args = {
-    			[1] = "9"
-    		}
-    
-    		game:GetService("ReplicatedStorage").Boxes.Core.Default.Remotes.Buy:InvokeServer(unpack(args))
+		local args = {
+			[1] = "9"
+		}
+
+		game:GetService("ReplicatedStorage").Boxes.Core.Default.Remotes.Buy:InvokeServer(unpack(args))
 	end	
-end
 end)
 
 
@@ -136,40 +133,40 @@ TextButton_3.TextSize = 14.000
 TextButton_3.TextWrapped = true
 TextButton_3.MouseButton1Down:connect(function()
 	toggle3 = not toggle3
+	if toggle3 == true then
+		TextButton_3.BackgroundColor3 = Color3.fromRGB(0, 255, 0)
+	end
 	if toggle3 == false then
 		TextButton_3.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
 	end
 	while toggle3 == true do
-	    task.wait()
-	    local huen = tick() % t / t
-	    local colorn = Color3.fromHSV(huen,1,1)
-	    if toggle3 == false then
-		    TextButton_3.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
-		else
-    	    TextButton_3.BackgroundColor3 = colorn
-    		local args = {
-    			[1] = "walkSpeed"
-    		}
-    
-    		game:GetService("ReplicatedStorage").Upgrades.Core.Default.Remotes.Upgrade:InvokeServer(unpack(args))
-    
-    
-    
-    		local args = {
-    			[1] = "itemSpeed"
-    		}
-    
-    		game:GetService("ReplicatedStorage").Upgrades.Core.Default.Remotes.Upgrade:InvokeServer(unpack(args))
-    
-    
-    
-    		local args = {
-    			[1] = "shootDistance"
-    		}
-    
-    		game:GetService("ReplicatedStorage").Upgrades.Core.Default.Remotes.Upgrade:InvokeServer(unpack(args))
-	    end
-    end
+		local args = {
+			[1] = "walkSpeed"
+		}
+
+		game:GetService("ReplicatedStorage").Upgrades.Core.Default.Remotes.Upgrade:InvokeServer(unpack(args))
+
+
+
+		local args = {
+			[1] = "itemSpeed"
+		}
+
+		game:GetService("ReplicatedStorage").Upgrades.Core.Default.Remotes.Upgrade:InvokeServer(unpack(args))
+
+
+
+		local args = {
+			[1] = "shootDistance"
+		}
+
+		game:GetService("ReplicatedStorage").Upgrades.Core.Default.Remotes.Upgrade:InvokeServer(unpack(args))
+	end
 end)
+
+
+
+
+
 
 UICorner_5.Parent = TextButton_3
